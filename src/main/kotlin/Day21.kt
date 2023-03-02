406d6017c5a7da54857f1318b35b2eb647e15985
johannesrave <johannes.rave@gmail.com>
2023-03-02 11:27:42 +0100

    Day21 B solved

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -2,77 +2,42 @@ import kotlin.properties.Delegates
 import kotlin.system.measureTimeMillis
 
 fun main() {
-//    val day21 = Day21("input/21_test.txt")
     val day21 = Day21("input/21.txt")
-//    measureTimeMillis {
-//        day21.solveA().also { result -> println(result) }
-//        // 22382838633806
-//    }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
+    measureTimeMillis {
+        day21.solveA().also { result -> println(result) }
+        // 22382838633806
+    }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
 
     measureTimeMillis {
         day21.solveB().also { result -> println(result) }
         // 3099532691300
     }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
-
 }
 
 class Day21(inputFileName: String) : Day(inputFileName) {
-    //algo:
-    // build tree of monkeys including dependencies:
-    //  parse all monkeys
-    //  iterate over them, injecting references to dependencies into all monkeys
-    // traverse tree depth first (using a stack)
-    // enter monkey:
-    //  does it have a number? return it
-    // if not:
-    //      enter left monkey
-    //      enter right monkey
-    //      operate and return
-    // idea: could already recurse while parsing...?
-
     fun solveA(input: String = this.input): Any {
         val monkeys = Monkey.parseFrom(input)
         val root = monkeys["root"]!!
-        val result = root.getResult()
-        return result
+        root.calculateNumbersForAllMonkeys()
+        return root.number
     }
-
-
-    // algo:
-    // start at root monkey, picking the left value as a base for the right path and vice versa
-    // while traversing, remember operations, and reverse them as the result of the humn monkey
-
-    // 6 == x
-    // x = 3 * y -> y = x / 3
-    // y = 1 + z -> z = y - 1
-    // z = ? -> z = 1
-
 
     fun solveB(input: String = this.input): Long? {
         val monkeys = Monkey.parseFrom(input)
         val root = monkeys["root"]!! as Monkey.OpMonkey
-        root.getResult() // to calculate and set all Monkey numbers
-        return root.right.digForHumn(root.left.number) ?: root.left.digForHumn(root.right.number)
+        root.calculateNumbersForAllMonkeys() // to calculate and set all Monkey numbers
+        return root.right.replaceHumnNumber(root.left.number) ?: root.left.replaceHumnNumber(root.right.number)
     }
 
     abstract class Monkey(open val name: String) {
         abstract var number: Long
 
-        abstract fun getResult(): Long
-        abstract fun digForHumn(v: Long): Long?
+        abstract fun calculateNumbersForAllMonkeys(): Long
+        abstract fun replaceHumnNumber(v: Long): Long?
 
-        data class NumberMonkey(
-            override val name: String, override var number: Long
-        ) : Monkey(name) {
-            override fun getResult(): Long = number
-            override fun digForHumn(v: Long): Long? {
-                println(this)
-                println("stopping to dig at $name with $v")
-                return when (name) {
-                    "humn" -> v
-                    else -> null
-                }
-            }
+        data class NumberMonkey(override val name: String, override var number: Long) : Monkey(name) {
+            override fun calculateNumbersForAllMonkeys(): Long = number
+            override fun replaceHumnNumber(v: Long): Long? = if (name == "humn") v else null
         }
 
         data class OpMonkey(
@@ -84,10 +49,11 @@ class Day21(inputFileName: String) : Day(inputFileName) {
             lateinit var left: Monkey
             lateinit var right: Monkey
             override var number by Delegates.notNull<Long>()
-            override fun getResult(): Long {
-                val left = left.getResult()
-                val right = right.getResult()
+            override fun calculateNumbersForAllMonkeys(): Long {
+                val left = left.calculateNumbersForAllMonkeys()
+                val right = right.calculateNumbersForAllMonkeys()
 
+                // set/save the number for all OpMonkeys visited by the tree traversal
                 number = when (op) {
                     "+" -> (left + right)
                     "-" -> (left - right)
@@ -95,41 +61,22 @@ class Day21(inputFileName: String) : Day(inputFileName) {
                     "/" -> (left / right)
                     else -> throw IllegalArgumentException("$op is not a valid MonkeyOperator!")
                 }
-                return number.also {
-                    println(
-                        "$name: ${
-                            left.toString().padStart(15, '_')
-                        } $op ${
-                            right.toString().padStart(15, '_')
-                        } = ${
-                            it.toString().padStart(15, '_')
-                        }"
-                    )
-                }
+                return number
             }
 
-            override fun digForHumn(v: Long): Long? {
-                println(this)
+            override fun replaceHumnNumber(v: Long): Long? {
                 return when (op) {
-                    "+" -> {
-                        right.digForHumn(v - left.number).also { println("right: $v - ${left.number}") }
-                            ?: left.digForHumn(Math.subtractExact(v, right.number)).also { println("left: $v - ${right.number}") }
-                    }
+                    "+" -> right.replaceHumnNumber(v - left.number)
+                        ?: left.replaceHumnNumber(Math.subtractExact(v, right.number))
 
-                    "-" -> {
-                        right.digForHumn(left.number - v).also { println("right: ${left.number} - $v") }
-                            ?: left.digForHumn(Math.addExact(v, right.number)).also { println("left: $v + ${right.number}") }
-                    }
+                    "-" -> right.replaceHumnNumber(left.number - v)
+                        ?: left.replaceHumnNumber(Math.addExact(v, right.number))
 
-                    "*" -> {
-                        right.digForHumn(v / left.number).also { println("right: $v / ${left.number}") }
-                            ?: left.digForHumn(Math.divideExact(v, right.number)).also { println("left: $v / ${right.number}") }
-                    }
+                    "*" -> right.replaceHumnNumber(v / left.number)
+                        ?: left.replaceHumnNumber(Math.divideExact(v, right.number))
 
-                    "/" -> {
-                        right.digForHumn(left.number / v).also { println("right: ${left.number} / $v") }
-                            ?: left.digForHumn(Math.multiplyExact(v, right.number)).also { println("left: $v * ${right.number}") }
-                    }
+                    "/" -> right.replaceHumnNumber(left.number / v)
+                        ?: left.replaceHumnNumber(Math.multiplyExact(v, right.number))
 
                     else -> throw IllegalArgumentException("$op is not a valid MonkeyOperator!")
                 }
@@ -137,41 +84,30 @@ class Day21(inputFileName: String) : Day(inputFileName) {
         }
 
         companion object {
-            fun parseFrom(input: String): Map<String, Monkey> {
-                val pattern =
-                    "(?<name>\\w+): (?:(?<number>\\d+)|(?<leftName>\\w+) (?<op>[+*/-]) (?<rightName>\\w+))".toRegex()
+            val pattern =
+                "(?<name>\\w+): (?:(?<number>\\d+)|(?<leftName>\\w+) (?<op>[+*/-]) (?<rightName>\\w+))".toRegex()
 
-                return pattern.findAll(input).map { match ->
-                    when {
-                        match.groups["number"] != null -> NumberMonkey(
-                            name = match.groups["name"]?.value!!,
-                            number = match.groups["number"]?.value?.toLong()!!,
-                        )
+            fun parseFrom(input: String): Map<String, Monkey> = pattern.findAll(input).map { match ->
+                val (name, number, op, leftName, rightName) = listOf("name", "number", "op", "leftName", "rightName")
+                    .map { match.groups[it]?.value }
+                when {
+                    name != null && number != null ->
+                        NumberMonkey(name, number.toLong())
 
-                        else -> {
-                            val (name, op, leftName, rightName) = listOf(
-                                "name",
-                                "op",
-                                "leftName",
-                                "rightName"
-                            ).map {
-                                match.groups[it]?.value
-                                    ?: throw IllegalArgumentException("$it is missing for a Monkey!")
-                            }
-                            OpMonkey(name, op, leftName, rightName)
-                        }
-                    }
-                }.associateBy { it.name }.apply {
-                    onEach { (name, monkey) ->
+                    name != null && op != null && leftName != null && rightName != null ->
+                        OpMonkey(name, op, leftName, rightName)
+
+                    else -> throw IllegalArgumentException("$name is missing args for bot NumberMonkey and OpMonkey!")
+                }
+            }.associateBy { it.name }
+                .apply {
+                    onEach { (_, monkey) ->
                         if (monkey is OpMonkey) {
-                            if (this.containsKey(monkey.leftName) || this.containsKey(monkey.rightName)) {
-                                monkey.left = this[monkey.leftName]!!
-                                monkey.right = this[monkey.rightName]!!
-                            } else throw IllegalArgumentException("Left ${monkey.leftName} or right ${monkey.rightName} are missing for a $name!")
+                            monkey.left = this[monkey.leftName]!!
+                            monkey.right = this[monkey.rightName]!!
                         }
                     }
                 }
-            }
         }
     }
 }
