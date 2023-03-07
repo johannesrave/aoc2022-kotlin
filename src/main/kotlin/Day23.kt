ac09d3b3724eef1b3389327726d03ce501306dc9
johannesrave <johannes.rave@gmail.com>
2023-03-07 01:04:34 +0100

    Day23 B solved

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -4,14 +4,18 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val day23 = Day23()
     measureTimeMillis {
-        day23.solveA("input/23_test.txt").also { result -> println(result) }
+        day23.solveB("input/23_test.txt").also { result -> println(result) }
     }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
 
-    measureTimeMillis {
-        day23.solveA("input/23.txt").also { result -> println(result) }
-    }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
+//    measureTimeMillis {
+//        day23.solveA("input/23.txt").also { result -> println(result) }
+//    }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
     // 4325 is too high
     // 4241
+
+    measureTimeMillis {
+        day23.solveB("input/23.txt").also { result -> println(result) }
+    }.also { elapsedTime -> println("Time taken: $elapsedTime ms") }
 }
 
 class Day23 {
@@ -100,6 +104,72 @@ class Day23 {
         board.onEach { println(it) }
 
         return board.sumOf { row -> row.count { it == emptyTile } }
+    }
+
+    fun solveB(inputFileName: String = "input/${this.javaClass.name.drop(3)}.txt"): Any? {
+
+        var board = File(inputFileName).readText(Charsets.UTF_8).toBoard()
+        val dirCycle = cycleDirs()
+        val elf = '#'
+        val emptyTile = '.'
+        val conflictedTarget = 'X'
+
+        var counter = 0
+        while (true) {
+            counter++
+            val dirs = dirCycle.next()
+
+            if (boardMustBeExpanded(board)) board = expandBoard(board)
+
+            // consider spacing and propose moves
+            board.onEachIndexed { y, row ->
+                row.onEachIndexed traverseRow@{ x, c ->
+                    if (c != elf) return@traverseRow
+                    //  check if elf should try to move at all
+                    val neighbouringElves = getAllNeighbours(x to y, board).any { it == elf }
+                    if (!neighbouringElves) return@traverseRow
+
+                    //  check which direction is a candidate for moving to
+                    val emptyDir = dirs.find { dir -> getNeighboursInDir(x to y, dir, board).none { it == elf } }
+                        ?: return@traverseRow
+
+                    // check if no other elf has proposed to walk there already
+                    val (nx, ny) = getCoordsInDir(x to y, emptyDir) // neighbour coords
+                    val neighbourInDir = board[ny][nx]
+                    when (neighbourInDir) {
+                        // if the tile is empty, flag it with the direction that this elf is in when seen
+                        // from it. this can later be used to swap the two tiles.
+                        emptyTile -> board[ny][nx] = emptyDir.getOpposing().toChar()
+                        // if the tile is marked with a Dir, the elf in that direction from it is
+                        // proposing to move there. setting this tile to 'conflicted' instead will also keep
+                        // them from moving.
+                        'N', 'S', 'W', 'E' -> board[ny][nx] = conflictedTarget
+                        // if the tile is already conflicted, don't propose to move
+                        conflictedTarget -> {}
+                    }
+                }
+            }
+
+            val noMovesMade = board.none { row -> listOf('N', 'S', 'W', 'E', 'X').any { c -> c in row } }
+            if (noMovesMade) return counter
+
+            // make moves by swapping proposed Dir tiles with the respective elves
+            board.onEachIndexed { y, row ->
+                row.onEachIndexed traverseRow@{ x, c ->
+                    when (c) {
+                        'N', 'S', 'W', 'E' -> {
+                            val dirOfElfMovingHere = Dir.valueOf(c.toString())
+                            val elfMovingHere = getCoordsInDir(x to y, dirOfElfMovingHere)
+                            elfMovingHere.let { (ex, ey) -> board[ey][ex] = emptyTile }
+                            board[y][x] = elf
+                        }
+
+                        'X' -> board[y][x] = emptyTile
+                    }
+                }
+            }
+        }
+        return null
     }
 
     private fun cycleDirs(): Iterator<List<Dir>> =
